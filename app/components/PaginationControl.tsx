1257d6887745d7bbbda9b37a1481ebc1813e27ee
Daniel Yadghar <daniel072.dy@gmail.com>
2024-08-23 23:23:39 +0330

    Be able to paginate

---
+++ app/components/PaginationControl.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Pagination } from "@nextui-org/react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+interface Props {
+  totalPage: number;
+  postCount: number;
+}
+
+const PaginationControl = ({ totalPage, postCount }: Props) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  if (totalPage <= 1) return null;
+
+  return (
+    <Pagination
+      showControls
+      size="lg"
+      className="p-8"
+      initialPage={1}
+      total={totalPage}
+      onChange={(value) => {
+        const newParams = new URLSearchParams(searchParams.toString());
+
+        if (totalPage >= 1) {
+          newParams.set("pageNumber", value.toString());
+        } else {
+          newParams.delete("pageNumber");
+        }
+
+        if (searchParams.get("search"))
+          newParams.set("search", searchParams.get("search")!);
+
+        if (searchParams.get("genreFilter"))
+          newParams.set("genreFilter", searchParams.get("genreFilter")!);
+
+        router.push(`?${newParams.toString()}`);
+      }}
+    />
+  );
+};
+
+export default PaginationControl;
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,33 @@
 import prisma from "@/prisma/client";
 import { Genre, Post } from "@prisma/client";
 import ActionBar from "./components/ActionBar";
+import PaginationControl from "./components/PaginationControl";
 import PostCard from "./components/PostCard";
 
 interface Props {
-  searchParams: { search: string; genreFilter: Genre };
+  searchParams: { search: string; genreFilter: Genre; pageNumber: number };
 }
 
-const Home = async ({ searchParams: { search, genreFilter } }: Props) => {
+const Home = async ({
+  searchParams: { search, genreFilter, pageNumber },
+}: Props) => {
   const genres = Object.values(Genre);
   const genre = genres.includes(genreFilter) ? genreFilter : undefined;
+
+  const pageSize: number = 4;
+  const currentPage: number = pageNumber || 1;
 
   const posts: Post[] = await prisma.post.findMany({
     where: {
       title: { contains: search },
       genre: { equals: genre },
     },
+    skip: (currentPage - 1) * pageSize,
+    take: pageSize,
+  });
+
+  const postCount = await prisma.post.count({
+    where: { genre, title: { contains: search } },
   });
 
   return (
@@ -33,6 +45,10 @@ const Home = async ({ searchParams: { search, genreFilter } }: Props) => {
           />
         ))}
       </div>
+      <PaginationControl
+        postCount={postCount}
+        totalPage={Math.ceil(postCount / pageSize)}
+      />
     </>
   );
 };
