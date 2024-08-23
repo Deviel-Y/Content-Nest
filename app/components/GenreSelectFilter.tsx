622f7b2376dfd291a1c29ba2d83cf0a420c314a6
Daniel Yadghar <daniel072.dy@gmail.com>
2024-08-23 19:51:42 +0330

    Be able to filter posts by genre

--- app/components/GenreSelectFilter.tsx
+++ app/components/GenreSelectFilter.tsx
@@ -2,6 +2,8 @@
 
 import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
 import { Genre } from "@prisma/client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { ReactElement } from "react";
 import {
   FcFilmReel,
   FcGraduationCap,
@@ -17,28 +19,66 @@ interface Props {
 }
 
 const GenreSelectFilter = ({ genres }: Props) => {
-  const genreMap: Record<Genre, { label: string; icon: any }> = {
-    HEALTH: { label: "Health", icon: <FcLike size={25} /> },
-    CULTURE_AND_ART: { label: "Culture & Art", icon: <FcFilmReel size={25} /> },
-    EDUCATION: { label: "Education", icon: <FcGraduationCap size={25} /> },
-    ENTERTAINMENT: { label: "Entertainment", icon: <FcHeadset size={25} /> },
-    NEWS: { label: "News", icon: <FcNews size={25} /> },
-    SPORTS: { label: "Sports", icon: <FcSportsMode size={25} /> },
-    TECHNOLOGY: { label: "Technology", icon: <FcSmartphoneTablet size={25} /> },
+  const router = useRouter();
+  const searchParam = useSearchParams();
+  const param = new URLSearchParams(searchParam);
+
+  type GenreType = Record<
+    Genre,
+    { label: string; value: Genre; icon: ReactElement }
+  >;
+
+  const genreMap: GenreType = {
+    HEALTH: { label: "Health", icon: <FcLike size={25} />, value: "HEALTH" },
+    CULTURE_AND_ART: {
+      label: "Culture & Art",
+      icon: <FcFilmReel size={25} />,
+      value: "CULTURE_AND_ART",
+    },
+    EDUCATION: {
+      label: "Education",
+      icon: <FcGraduationCap size={25} />,
+      value: "EDUCATION",
+    },
+    ENTERTAINMENT: {
+      label: "Entertainment",
+      icon: <FcHeadset size={25} />,
+      value: "ENTERTAINMENT",
+    },
+    NEWS: { label: "News", icon: <FcNews size={25} />, value: "NEWS" },
+    SPORTS: {
+      label: "Sports",
+      icon: <FcSportsMode size={25} />,
+      value: "SPORTS",
+    },
+    TECHNOLOGY: {
+      label: "Technology",
+      icon: <FcSmartphoneTablet size={25} />,
+      value: "TECHNOLOGY",
+    },
+  };
+
+  const handleChange = (value: string) => {
+    if (value.length === 0 && searchParam.size === 1) router.push("");
+
+    const selectedGenre = genres.find(
+      (genre) => genreMap[genre].label === value
+    );
+    if (selectedGenre) {
+      const genreValue = genreMap[selectedGenre].value;
+      router.push(`/?genreFilter=${genreValue}`);
+    }
   };
 
   return (
     <Autocomplete
+      onInputChange={handleChange}
       description="Filter post by their genres"
       variant="underlined"
       label="Genre Filter"
     >
       {genres.map((genre) => (
-        <AutocompleteItem
-          value={genre}
-          startContent={genreMap[genre].icon}
-          key={genre}
-        >
+        <AutocompleteItem startContent={genreMap[genre].icon} key={genre}>
           {genreMap[genre].label}
         </AutocompleteItem>
       ))}
--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,23 @@ import ActionBar from "./components/ActionBar";
 import PostCard from "./components/PostCard";
 
 interface Props {
-  searchParams: { search: string };
+  searchParams: { search: string; genreFilter: Genre };
 }
 
-const Home = async ({ searchParams: { search } }: Props) => {
-  const posts: Post[] = await prisma.post.findMany({
-    where: { title: { contains: search } },
-  });
-
+const Home = async ({ searchParams: { search, genreFilter } }: Props) => {
   const genres = Object.values(Genre);
+  const genre = genres.includes(genreFilter) ? genreFilter : undefined;
+
+  const posts: Post[] = await prisma.post.findMany({
+    where: {
+      title: { contains: search },
+      genre: { equals: genre },
+    },
+  });
 
   return (
     <>
-      <ActionBar genres={genres} />
+      <ActionBar genres={Object.values(Genre)} />
       <div className="grid grid-cols-2 max-md:grid-cols-1 gap-y-8 gap-x-10 px-5">
         {posts.map((post) => (
           <PostCard
