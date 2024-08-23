bf57a9d01cbb919d674c1baebcdf157781995311
Daniel Yadghar <daniel072.dy@gmail.com>
2024-08-23 11:07:12 +0330

    Make PostCard contents dynamic

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -1,31 +1,30 @@
 import { Button, Card, Link } from "@nextui-org/react";
 
-const PostCard = () => {
+interface Props {
+  createdAt: string;
+  title: string;
+  content: string;
+}
+
+const PostCard = ({ content, createdAt, title }: Props) => {
   return (
     <Card
       shadow="md"
       isBlurred
       className="flex flex-row max-md:flex-col max-md:h-auto h-64"
     >
-      <figure className="mb-3 p-3 flex justify-center align-middle h-full">
+      <figure className="p-3 flex justify-center align-middle h-full">
         <div className="bg-slate-400 w-56  max-md:w-[250px] max-md:h-[250px] rounded-2xl">
           Image
         </div>
       </figure>
 
       <div className="flex flex-col p-4 gap-y-2 w-full">
-        <p className="text-gray-400 text-sm max-lg:mt-2">date</p>
+        <p className="text-gray-400 text-sm max-lg:mt-2">{createdAt}</p>
 
-        <h1 className="font-bold text-[25px]">title</h1>
+        <h1 className="font-bold text-[25px]">{title}</h1>
 
-        <article className="flex-grow">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-          ratione incidunt ab, eveniet facilis odit ullam earum voluptatibus
-          voluptatem eaque fuga rem? Error reiciendis nulla est nobis aliquam
-          necessitatibus, aliquid voluptates officia commodi vitae impedit sunt
-          at magni doloremque id corrupti repellat nostrum libero excepturi qui
-          ex ut vel! Reiciendis.
-        </article>
+        <article className="flex-grow">{content}</article>
 
         <Button
           size={"md"}
