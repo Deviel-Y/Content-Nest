6c047b907f5a487902c99efd9306826ac382426e
Daniel Yadghar <daniel072.dy@gmail.com>
2024-08-24 23:50:56 +0330

    Make profile picture clickable

--- app/components/AuthenticationStatus.tsx
+++ app/components/AuthenticationStatus.tsx
@@ -1,6 +1,14 @@
-import { Avatar, Button, NavbarContent } from "@nextui-org/react";
+import {
+  Avatar,
+  Button,
+  Link,
+  NavbarContent,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import { Session } from "next-auth";
 import { useSession } from "next-auth/react";
-import Link from "next/link";
 
 const AuthenticationStatus = () => {
   const { data, status } = useSession();
@@ -10,24 +18,53 @@ const AuthenticationStatus = () => {
       {status === "unauthenticated" && (
         <Button
           as={Link}
+          href="/api/auth/signin"
           variant="shadow"
           color="primary"
-          href="/api/auth/signin"
         >
           Sign In
         </Button>
       )}
 
       {status === "authenticated" && (
-        <Avatar
-          isBordered
-          color="primary"
-          src={data.user?.image!}
-          showFallback={false}
-        />
+        <AvatarProfileControl sessionData={data} />
       )}
     </NavbarContent>
   );
 };
 
 export default AuthenticationStatus;
+
+export const AvatarProfileControl = ({
+  sessionData,
+}: {
+  sessionData: Session;
+}) => {
+  return (
+    <Popover shadow="lg" color="default">
+      <PopoverTrigger>
+        <Avatar
+          className="hover:scale-110 transition-all cursor-pointer"
+          isBordered
+          color="primary"
+          src={sessionData.user?.image!}
+          showFallback={false}
+        />
+      </PopoverTrigger>
+      <PopoverContent>
+        {() => (
+          <div className="flex flex-col gap-y-1 m-2">
+            <p className="text-lg">{sessionData.user?.name}</p>
+            <p className="text-gray-500">{sessionData.user?.email}</p>
+            <Button className="mt-3 mb-1" size="sm" color="primary">
+              Update Profile
+            </Button>
+            <Button size="sm" color="danger" variant="ghost">
+              Sign Out
+            </Button>
+          </div>
+        )}
+      </PopoverContent>
+    </Popover>
+  );
+};
