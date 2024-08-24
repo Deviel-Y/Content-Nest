db0d1443a5a605dc2506b536b6ee5f37af9b357f
Daniel Yadghar <daniel072.dy@gmail.com>
2024-08-25 00:38:06 +0330

    Build SignOutConfirmation component

--- app/components/AuthenticationStatus.tsx
+++ app/components/AuthenticationStatus.tsx
@@ -2,13 +2,20 @@ import {
   Avatar,
   Button,
   Link,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
   NavbarContent,
   Popover,
   PopoverContent,
   PopoverTrigger,
+  useDisclosure,
 } from "@nextui-org/react";
 import { Session } from "next-auth";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { useState } from "react";
 
 const AuthenticationStatus = () => {
   const { data, status } = useSession();
@@ -40,8 +47,15 @@ export const AvatarProfileControl = ({
 }: {
   sessionData: Session;
 }) => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState<boolean>(false);
+
   return (
-    <Popover shadow="lg" color="default">
+    <Popover
+      isOpen={isPopoverOpen}
+      onOpenChange={() => setIsPopoverOpen(!isPopoverOpen)}
+      shadow="lg"
+      color="default"
+    >
       <PopoverTrigger>
         <Avatar
           className="hover:scale-110 transition-all cursor-pointer"
@@ -59,12 +73,49 @@ export const AvatarProfileControl = ({
             <Button className="mt-3 mb-1" size="sm" color="primary">
               Update Profile
             </Button>
-            <Button size="sm" color="danger" variant="ghost">
-              Sign Out
-            </Button>
+            <SignOutConfirmation />
           </div>
         )}
       </PopoverContent>
     </Popover>
+  );
+};
+
+export const SignOutConfirmation = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Button size="sm" color="danger" variant="ghost" onPress={onOpen}>
+        Sign Out
+      </Button>
+      <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col">
+                Sign Out Confirmation
+              </ModalHeader>
+              <ModalBody>
+                <p>Are you sure you want to sign out?</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  size="sm"
+                  color="primary"
+                  variant="solid"
+                  onPress={onClose}
+                >
+                  Close
+                </Button>
+                <Button size="sm" color="danger" onPress={() => signOut()}>
+                  Sign Out
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
