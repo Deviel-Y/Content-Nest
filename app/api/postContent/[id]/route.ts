6857c6486e153d708e08e7f01899724e8e058fc9
Daniel Yadghar <daniel072.dy@gmail.com>
2024-09-10 19:28:51 +0330

    Build a DELETE API to delete a post

--- app/api/postContent/[id]/route.ts
+++ app/api/postContent/[id]/route.ts
@@ -6,36 +6,42 @@ interface Props {
   params: { id: string };
 }
 
+export const DELETE = async (
+  request: NextRequest,
+  { params: { id } }: Props
+) => {
+  const post = await prisma.post.findUnique({ where: { id } });
+  if (!post) return NextResponse.json("Post not found", { status: 404 });
+
+  const deletedPost = await prisma.post.delete({ where: { id } });
+  return NextResponse.json(
+    `Post with title of ${deletedPost.title} has been deleted`
+  );
+};
+
 export const PATCH = async (
   request: NextRequest,
   { params: { id } }: Props
 ) => {
   try {
-    // Parse the body
     const body: PostSchemaType = await request.json();
     const { content, genre, imageUrl, shortDescription, title } = body;
 
-    // Check if the post exists
     const post = await prisma.post.findUnique({ where: { id } });
     if (!post)
       return NextResponse.json({ message: "Post not found" }, { status: 404 });
 
-    // Validate the incoming data
     const validation = postSchema.safeParse(body);
     if (!validation.success)
       return NextResponse.json(validation.error.format(), { status: 400 });
 
-    // Update the post
     const updatedPost = await prisma.post.update({
       where: { id },
       data: { content, genre, imageUrl, shortDescription, title },
     });
 
-    // Return the updated post or a success message
     return NextResponse.json(updatedPost, { status: 200 });
   } catch (error) {
-    // Handle errors more gracefully
-    console.error("Error updating post:", error);
     return NextResponse.json(
       { message: "Internal Server Error" },
       { status: 500 }
